84553957b9879540614e784c901c45e7809a2460
3120004851叶宇帆 <2365259136@qq.com>
2022-09-16 16:11:55 +0800

    第一次提交

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,26 @@
-public class Main {
+import com.hankcs.hanlp.HanLP;
 
-    public static void main(String[] args) {
-        System.out.println("小黑子露出鸡脚了吧！");
+import java.io.IOException;
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class Main
+{
+    public static void main(String[] args) throws IOException
+    {
+        String str0 = Txt.readTxt(args[0]);
+        String str1 = Txt.readTxt(args[1]);
+        String resultFileName = args[2];
+        // 由字符串得出对应的 simHash值
+        String simHash0 = SimHash.getSimHash(str0);
+        String simHash1 = SimHash.getSimHash(str1);
+        // 由 simHash值求出相似度
+        double similarity = Hamming.getSimilarity(simHash0, simHash1);
+        // 把相似度写入最后的结果文件中
+        Txt.writeTxt(similarity, resultFileName);
+        // 退出程序
+        System.exit(0);
     }
 }
